3ea876f55ad79abad99df75bcdcbfa9cfe07929d
nccherian <30857630+nccherian@users.noreply.github.com>
2017-08-09 12:41:47 +0530

    Add files via upload

---
+++ SeamEdu/SeamEdu/Node.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ag.student
+ * Date: 01-05-2017
+ * Time: 05:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SeamEdu
+{
+	public class Node
+	{
+		private int data;
+		private Node left;
+		private Node right;
+		
+		public Node Left
+		{
+			get
+			{
+				return this.left;
+			}
+			set
+			{
+				this.left = value;
+			}
+		}
+		
+		public Node Right
+		{
+			get 
+			{
+				return this.right;
+			}
+			set 
+			{
+				this.right = value;
+			}
+		}
+		
+		public int Data //getter for input data
+		{
+			get
+			{
+				return data;
+			}
+		}
+		
+		public Node(int data) //Constructor
+		{
+			this.data = data;
+		}
+	}
+}
---
+++ SeamEdu/SeamEdu/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ag.student
+ * Date: 01-05-2017
+ * Time: 05:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace SeamEdu
+{
+	public class Program
+	{
+		
+		public static void Main (string[]args)
+		{
+			Node rootNode = null; //Temporary variable 
+			
+			Console.WriteLine("Please Enter the values in integers and press 'q' to quit \n");
+
+			while(true) //endless loop
+			{
+				String inputData = Console.ReadLine();
+				if(String.Equals(inputData,"q"))
+				{
+					break;
+				}
+				try
+				{
+					int result = Convert.ToInt32(inputData);
+					Node node = new Node(result);
+					
+					if(rootNode==null) 
+					{
+						rootNode = node;
+					}
+					else
+					{
+						Node currentNode;
+						currentNode = rootNode;
+						
+						while()
+						{
+							if(currentNode.Data > node.Data)
+							{
+								if(currentNode.Left == null)
+								{
+									currentNode.Left = node;
+									//Console.WriteLine("Left"+currentNode.Left.Data);
+								}
+								else
+								{
+									currentNode = currentNode.Left;
+								}
+							}
+							else{
+								
+							}
+						}
+					}
+					
+					Console.WriteLine("Root Node is: " + rootNode.Data);
+					Console.WriteLine("Current Node is: " + node.Data);
+					
+				} 
+				catch(Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+					
+		}
+	}
+}
